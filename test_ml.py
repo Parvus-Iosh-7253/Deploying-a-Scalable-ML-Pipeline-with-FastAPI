88f731dd0c898032a631f4fe33655f0e4180c58f
Josh Miller <j.wakeling98@gmail.com>
2026-04-23 19:44:55 -0600

    Implemented three unit tests in test_ml.py

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,42 @@
 import pytest
-# TODO: add necessary import
+from train_model import data, X_train, X_test, model, p, r, fb
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_model_metrics_values():
     """
-    # add description for the first test
+    Test to ensure all metrics are correctly calculated.
+    Expected:
+    - all score values should be between 0 and 1 inclusive
     """
-    # Your code here
-    pass
 
-
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
-    """
-    # add description for the second test
-    """
-    # Your code here
-    pass
+    assert 0.0 <= p <= 1.0
+    assert 0.0 <= r <= 1.0
+    assert 0.0 <= fb <= 1.0
 
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_training_testing_size():
     """
-    # add description for the third test
+    Test to ensure training and testing data are of correct dimensions.
+    Expected:
+    - Both are 108 columns wide after being encoded (done at creation in the process_data function)
+    - X_train should have a number of rows = 80% of the raw data
+    - X_test should have a number of rows = 20% of the raw data
     """
-    # Your code here
-    pass
+
+    assert X_train.shape[1] == 108
+    assert X_test.shape[1] == 108
+    
+    test_size = int(data.shape[0] * 0.2)
+    train_size = data.shape[0] - test_size
+
+    assert round(X_train.shape[0] / data.shape[0], 1) == 0.8
+    assert round(X_test.shape[0] / data.shape[0], 1) == 0.2
+
+
+def test_model_type():
+    """
+    Test to ensure ML model is of the expected build.
+    Expected:
+    - model type is RandomForestClassifier
+    """
+    
+    assert model.__class__.__name__ == "RandomForestClassifier"
